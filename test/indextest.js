7a4c67f7a535694549fc6e915f0aef74ef662328
Eileen Wei <eileenwei0105@gmail.com>
2015-11-04 00:36:58 -0800

    Adding supertest to test route

--- test/indextest.js
+++ test/indextest.js
@@ -1,20 +1,31 @@
 var express = require('express');
 var expect = require('expect.js');
+var request = require('supertest');
+var should = require('should');
 var indexRoutes = require('.././routes/index.js');
 
+var app = express();
 var url = 'http://localhost:8888';
-
 console.log("start testing!");
 
-suite('Index Testing', function(){
-  test('Index Route', function(done){
-	/*describe('index page', function() {
-    it("renders successfully", function(done) {
-        request(url).get('/').expect(200, done);    
-      })
-  });*/
-		done();
+//suite('Route Testing', function(){
+describe('Index Route Testing', function(){
+	it('Index Route', function(done){
+		request(url)
+  	.get('/')
+  	.expect('Content-Type', 'text/html')
+		.end(function(err, res){
+			done();
+		 });
+	});
 });
-  
-});
+//});
+
+
+
+
+
+
+
+
 
