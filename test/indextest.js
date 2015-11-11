2cb416ae7f456f1ddc4bca6c9d129dff1661a9ff
deniseteng1125 <denise1125@hotmail.com>
2015-11-10 22:08:12 -0800

    update route in indextest

--- test/indextest.js
+++ test/indextest.js
@@ -2,7 +2,9 @@ var express = require('express');
 var expect = require('expect.js');
 var request = require('supertest');
 var should = require('should');
-var indexRoutes = require('.././routes/index.js');
+var indexRoutes = require('.././controllers/index.js');
+var search = require('.././controllers/searchController.js');
+
 
 var app = express();
 var url = 'http://localhost:8888';
@@ -12,10 +14,27 @@ console.log("start testing!");
 suite('Index Route Testing', function(){
 	test('Index Route', function(done){
 		request(url)
-  	.get('/')
-  	.expect('Content-Type', 'text/html')
+		.get('/')
+		.expect('Content-Type', 'text/html')
 		.end(function(err, res){
-			done();
+		done();
 		 });
 	});
+	
+	
+	
+	test('Search testing', function(done){
+		/*search test = new search();
+		expect(test.fetchSearchResults()).to.be.ok();
+		done();*/
+		request(url)
+		.get('/')
+		.expect('Content-Type', 'text/html')
+		.end(function(err, res){
+		done();
+		 });
+	});
+	
+	
+	
 });
